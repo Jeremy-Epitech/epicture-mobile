7ecdbd9f0296e1c8281032702a008797bc85464a
Jeremy Nayet <71263473+Jeremy-Epitech@users.noreply.github.com>
2021-03-16 14:30:53 +0100

    Get ok mais affichage Ko
    Récupération des img de section hot et trier par viral

--- src/home.js
+++ src/home.js
@@ -1,56 +1,81 @@
-import React, { Component } from 'react';
-import { Text, Alert, Button, View, StyleSheet, TextInput } from 'react-native';
-import { env } from 'yargs';
+import React, { Component, useState } from 'react';
+import { Text, Form, Button, View, StyleSheet, TextInput } from 'react-native';
+import axios from 'axios';
+import env from '../variables';
+
 
 export default class home extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            imgur: '',
+            images: [],
         };
     }
 
-    go = () => {
-        const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if (reg.test(this.state.email) === true) {
-            alert('valid');
-        }
-        else {
-            alert();
-        }
-
+    componentDidMount() {
+        this.callImgur();
     }
 
-    async callImgur(tag) {
-        console.log(tag)
-        const uri = `https://api.imgur.com/3/gallery/t/${tag}`;
-        await axios.get(uri, {
+
+    async callImgur() {
+        await axios.get(`${env.dev.apiUrl}/3/gallery/hot/viral`, {
             headers: {
-                Authorization: `ClientID${ENV.CLIENT_ID}`
+                Authorization: `Client-ID ${env.client.CLIENT_ID}`
             }
-        }).then((response) => {
-            console.log(response)
+        }).then(response => {
+            this.pushImgs(response.data.data);
+        }).catch(err => {
+            console.log(err)
         });
-    }
+    };
+
+    pushImgs(res) {
+        res.forEach(element => {
+            let img;
+            if (element.images) {
+                element.images.forEach(image => {
+                    img = {
+                        description: image.title,
+                        img: image.link
+                    }
+                })
+            } else {
+                img = {
+                    description: '',
+                    img: element.link
+                }
+            }
+            const imgData = {
+                title: element.title,
+                imgs: img,
+                date: element.datetime
+            };
+            this.state.images.push(imgData)
+        })
+    };
 
     render() {
+        const imgList = this.state.images.map((title) => {
+            return (
+                <View><Text>{title}</Text></View>
+            )
+        })
         return (
-            <h1>ezzabu</h1>
-            // <View style={styles.container}>
-            //     <View style={styles.inputContainer}>
-            //         <Form>
-            //             <Input
-            //                 placeholder="Tag of imgur"
-            //                 style={styles.input}
-            //                 onSubmit={event => callImgur(event.target.value)}
-            //             />
-            //         </Form>
-            //     </View>
-            //     <View>
-            //         {imgur.map((title) => <Text>{title}</Text>)}
-            //     </View>
-            // </View>
+            // <h1>ezzabu</h1>
+            <View style={styles.container}>
+                <View style={styles.input}>
+                    <TextInput
+                        placeholder="Tag of imgur"
+                        style={styles.inputext}
+                        onSubmit={event => callImgur(event.target.value)}
+                    />
+                </View>
+                {imgList}
+                {/* <View>
+                {images.map((title) => <Text>{title}</Text>)}
+            </View> */}
+            </View>
         );
     }
 }
@@ -63,21 +88,24 @@ const styles = StyleSheet.create({
         backgroundColor: '#5f5f5f',
     },
     input: {
-        width: 200,
-        height: 44,
-        padding: 10,
-        borderWidth: 1,
-        borderColor: 'black',
-        marginBottom: 10,
+        // width: 200,
+        // height: 44,
+        // padding: 3,
+        textAlign: 'center',
+        // borderWidth: 1,
+        // borderColor: 'black',
+        // marginBottom: 10,
+        maringTop: 5,
     },
     inputext: {
         width: 200,
         height: 44,
         padding: 10,
-        textAlign: 'center',
         fontWeight: 'bold',
         borderWidth: 1,
         borderColor: 'black',
+        backgroundColor: 'white',
         marginBottom: 10,
+        borderRadius: 25,
     },
 });
